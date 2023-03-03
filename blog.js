b79e8712e0a546fb33d5d9795dcfd4e3fec66d97
Anderwander <ander1993trabajo@hotmail.com>
2023-03-03 13:46:35 +0100

    meto más función

--- blog.js
+++ blog.js
@@ -1,6 +1,7 @@
 //evento para submit
 let form = document.getElementById("blogForm");
 form.addEventListener("submit", createPost);
+document.getElementById("blogForm").reset();
 
 function createPost(event) {
   event.preventDefault();
@@ -9,24 +10,75 @@ function createPost(event) {
   let h3 = document.createElement("h3");
   let p = document.createElement("p");
   let article = document.createElement("article");
-  let icono = document.createElement("i");
-  icono.classList.add("fa", "fa-trash");
+  let iconoBorrar = crearIcono("fa-trash", deletePost);
+  let iconoEditar = crearIcono("fa-pencil", editPost);
   h3.innerText = titulo;
   p.innerText = contenido;
   article.appendChild(h3);
   article.appendChild(p);
-  article.appendChild(icono);
+  article.appendChild(iconoEditar);
+  article.appendChild(iconoBorrar);
   nuevosBlog.insertBefore(article, nuevosBlog.children[1]);
-  icono.addEventListener("click", deletePost);
-  document.getElementById("blogForm").reset();
+  iconoBorrar.addEventListener("click", deletePost);
+  iconoEditar.addEventListener("click", editPost);
 }
 
 function deletePost(event) {
   let element = event.target;
   let parent = element.parentElement;
-  parent.remove();
   let text = parent.getElementsByTagName("h3")[0].innerText;
   if (confirm("¿deseas borrar este elemento? \n" + text)) {
     parent.remove();
   }
+}
+
+function cancelEdit(event, textoTitulo, textoParrafo) {
+  let element = event.target;
+  let parent = element.parentElement;
+  let titulo = document.createElement("h3");
+  let parrafo = document.createElement("p");
+  titulo.innerText = textoTitulo;
+  parrafo.innerText = textoParrafo;
+  parent.appendChild(titulo);
+  parent.appendChild(parrafo);
+
+  parent.getElementsByTagName("input")[0].remove();
+  parent.getElementsByTagName("textArea")[0].remove();
+  let iconoEditar = crearIcono("fa-pencil", editPost);
+  crearIcono("fa-pencil", editPost);
+  parent.appendChild(iconoEditar);
+  element.remove();
+}
+
+function crearIcono(simbolo, callback) {
+  let icono = document.createElement("i");
+  icono.classList.add("fa", simbolo);
+  icono.addEventListener("click", callback);
+  return icono;
+}
+
+function editPost(event) {
+  let element = event.target;
+  let parent = element.parentElement;
+  let titulo = parent.getElementsByTagName("h3")[0].innerText;
+  let texto = parent.getElementsByTagName("p")[0].innerText;
+  let inputTitulo = document.createElement("input");
+  let br = document.createElement("br");
+  let textArea = document.createElement("textarea");
+  let iconoCancelar = crearIcono("fa-close", function (event) {
+    cancelEdit(event, titulo, texto);
+  });
+  element.remove();
+
+  inputTitulo.setAttribute("type", "text");
+  inputTitulo.value = titulo;
+  textArea.value = texto;
+
+  parent.getElementsByTagName("h3")[0].remove();
+  parent.getElementsByTagName("p")[0].remove();
+
+  parent.appendChild(inputTitulo);
+  parent.appendChild(br);
+  parent.appendChild(textArea);
+  parent.appendChild(iconoCancelar);
 }
